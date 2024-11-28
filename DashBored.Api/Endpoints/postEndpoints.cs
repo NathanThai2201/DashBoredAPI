bc3a6c0ee41d3b8910f7a51167a169ecb51ff25f
NathanThai2201 <thainhatanh2201@gmail.com>
2024-11-27 20:34:20 -0700

    8.0 migrate

--- DashBored.Api/Endpoints/postEndpoints.cs
+++ DashBored.Api/Endpoints/postEndpoints.cs
@@ -10,19 +10,26 @@ namespace DashBored.Api.Endpoints;
 public static class postEndpoints
 {
     const string getPostEndpointName = "getPost";
-    private static readonly List<postDTO> posts = [
-        new (
+
+    // Fix the list initialization
+    private static readonly List<postDTO> posts = new List<postDTO>
+    {
+        new postDTO(
             1,
             "Post1",
             "Desc1",
-            new DateOnly(2024,1,2)
-        ), new (
+            new DateOnly(2024, 1, 2)
+        ),
+        new postDTO(
             2,
             "Post2",
             "Desc2",
-            new DateOnly(2020,2,10)
-        )];
-    public static RouteGroupBuilder mapPostEndpoints(this WebApplication app){
+            new DateOnly(2020, 2, 10)
+        )
+    };
+
+    public static RouteGroupBuilder mapPostEndpoints(this WebApplication app)
+    {
         // CRUD ENDPOINTS
 
         var group = app.MapGroup("posts")
@@ -45,7 +52,6 @@ public static class postEndpoints
         // POST  /posts
         group.MapPost("/", async (createpostDTO newPost, DashBoredContext dbContext) => 
         {
-
             Post post = new()
             {
                 Name = newPost.Name,
@@ -56,7 +62,7 @@ public static class postEndpoints
             dbContext.Posts.Add(post);
             await dbContext.SaveChangesAsync();
             
-            return Results.CreatedAtRoute(getPostEndpointName, new {id = post.Id}, post);
+            return Results.CreatedAtRoute(getPostEndpointName, new { id = post.Id }, post);
         });
         
         // PUT  /posts/1
@@ -79,7 +85,7 @@ public static class postEndpoints
         // DELETE /posts/1
         group.MapDelete("/{id}", async (int id, DashBoredContext dbContext) => 
         {
-           await dbContext.Posts.Where(Post => Post.Id == id).ExecuteDeleteAsync();
+            await dbContext.Posts.Where(post => post.Id == id).ExecuteDeleteAsync();
 
             return Results.NoContent();
         });
