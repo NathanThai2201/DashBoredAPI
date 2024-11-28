e2e4dfce93045dfe8bbe46f749350944f7b9af93
NathanThai2201 <thainhatanh2201@gmail.com>
2024-11-27 21:26:55 -0700

    update

--- DashBored.Api/Program.cs
+++ DashBored.Api/Program.cs
@@ -6,6 +6,11 @@ using Microsoft.EntityFrameworkCore;
 //SQLitePCL.Batteries.Init();
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://*:{port}");
+
+builder.Services.AddHealthChecks();
+
 var connectionString = ConnectionHelper.GetConnectionString();
 builder.Services.AddEntityFrameworkNpgsql().AddDbContext<DashBoredContext>(
    options => options.UseNpgsql(connectionString)
@@ -13,6 +18,7 @@ builder.Services.AddEntityFrameworkNpgsql().AddDbContext<DashBoredContext>(
 
 
 var app = builder.Build();
+app.UseHealthChecks("/health");
 app.mapPostEndpoints();
 await app.MigrateDbAsync();
 
